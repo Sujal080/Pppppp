04b3c0a14b30598e25e57027e55ac9ab93b0a1c8
mdrobot3115 <116418622+mdrobot3115@users.noreply.github.com>
2024-10-09 10:41:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 28660719
-api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
-bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
-auth_users = "6990313439"
+api_id = ""
+api_hash = ""
+bot_token = ""
+auth_users = ""
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6990313439"
+osowner_users = ""
 owner_users = [int(num) for num in osowner_users.split(",")]
