f24bbcfbfe2d58d7fb1c606f813942dcda050012
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-06-07 17:27:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = ""
-api_hash = ""
-bot_token = ""
-auth_users = ""
+api_id = "26330942"
+api_hash = "5de9fd033aa828dfd3bf0c28adeee660"
+bot_token = "7460074981:AAH3xfM8LwbSaxqh9HMW_1QKfYXSHKZBRU0"
+auth_users = "6883471516"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = ""
+osowner_users = "6883471516"
 owner_users = [int(num) for num in osowner_users.split(",")]
